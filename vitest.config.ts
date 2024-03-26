ce2bad7bf5357889a014284959e24b111da9d76d
Dani Akash <DaniAkash@users.noreply.github.com>
2024-03-26 17:09:31 +0530

    ci: setup tests on pr (#2)
    * ci: setup tests on pr
    
    * ci: add env for tests
    
    * ci: fix invalid characters in artifacts
    
    * ci: use clean branch name for reading artifacts
    
    * ci: added json-summary to coverage reporters
    
    * ci: set coverage thresholds

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,12 @@ export default defineConfig({
   test: {
     coverage: {
       reporter: ["text", "json", "html", "clover", "json-summary"],
+      thresholds: {
+        lines: 60,
+        branches: 60,
+        functions: 60,
+        statements: 60,
+      },
     },
   },
 });
