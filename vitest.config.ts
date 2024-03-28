2a2dac08cd26ecb3a2789967424f1643a2a42ff8
DaniAkash <dani@clarifai.com>
2024-03-28 13:32:08 +0530

    chore: remove coverage threshold

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,12 +4,6 @@ export default defineConfig({
   test: {
     coverage: {
       reporter: ["text", "json", "html", "clover", "json-summary"],
-      thresholds: {
-        lines: 60,
-        branches: 60,
-        functions: 60,
-        statements: 60,
-      },
     },
   },
 });
