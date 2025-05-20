053a2326c9a28d31e7d2a4831cc7856ae2f6342d
DaniAkash <dani@clarifai.com>
2025-05-20 18:59:33 +0530

    feat: support complex data types in payload constructor

--- src/utils/constructPartsFromPayload.ts
+++ src/utils/constructPartsFromPayload.ts
@@ -1,13 +1,20 @@
 import {
+  Audio,
+  Concept,
   Data,
+  Frame,
+  Image,
   ModelTypeField,
   Part,
+  Region,
+  Video,
 } from "clarifai-nodejs-grpc/proto/clarifai/api/resources_pb";
 import {
   JavaScriptValue,
   Struct,
 } from "google-protobuf/google/protobuf/struct_pb";
 import { setPartDataTypes } from "./setPartsFromParams";
+import { fromPartialProtobufObject } from "./fromPartialProtobufObject";
 
 export const constructPartsFromPayload = (
   payload: Record<string, JavaScriptValue> | JavaScriptValue[],
@@ -70,8 +77,86 @@ export const constructPartsFromPayload = (
 
     let updatedFieldValue = fieldValue;
 
-    if (typeof fieldValue === "object" && !Array.isArray(fieldValue)) {
-      updatedFieldValue = JSON.stringify(fieldValue);
+    if (typeof fieldValue === "object" && fieldValue !== null) {
+      if (fieldType === ModelTypeField.DataType.JSON_DATA) {
+        updatedFieldValue = JSON.stringify(fieldValue);
+      }
+      if (
+        fieldType === ModelTypeField.DataType.IMAGE &&
+        !Array.isArray(fieldValue)
+      ) {
+        const imageData = fromPartialProtobufObject(Image, fieldValue);
+        data.setImage(imageData);
+        return;
+      }
+      if (fieldType === ModelTypeField.DataType.AUDIO) {
+        const audioData = fromPartialProtobufObject(Audio, fieldValue);
+        data.setAudio(audioData);
+        return;
+      }
+      if (fieldType === ModelTypeField.DataType.VIDEO) {
+        const videoData = fromPartialProtobufObject(Video, fieldValue);
+        data.setVideo(videoData);
+        return;
+      }
+      if (
+        fieldType === ModelTypeField.DataType.CONCEPT &&
+        !Array.isArray(fieldValue)
+      ) {
+        const conceptData = fromPartialProtobufObject(Concept, fieldValue);
+        data.setConceptsList([conceptData]);
+        return;
+      }
+      if (
+        fieldType === ModelTypeField.DataType.CONCEPT &&
+        Array.isArray(fieldValue)
+      ) {
+        const conceptsList = fieldValue.map((each) => {
+          return fromPartialProtobufObject(Concept, each);
+        });
+        data.setConceptsList(conceptsList);
+        return;
+      }
+      if (
+        fieldType === ModelTypeField.DataType.REGION &&
+        !Array.isArray(fieldValue)
+      ) {
+        const regionData = fromPartialProtobufObject(Region, fieldValue);
+        data.setRegionsList([regionData]);
+        return;
+      }
+      if (
+        fieldType === ModelTypeField.DataType.REGION &&
+        Array.isArray(fieldValue)
+      ) {
+        const regionsList = fieldValue.map((each) => {
+          return fromPartialProtobufObject(Region, each);
+        });
+        data.setRegionsList(regionsList);
+        return;
+      }
+      if (
+        fieldType === ModelTypeField.DataType.FRAME &&
+        !Array.isArray(fieldValue)
+      ) {
+        const frameData = fromPartialProtobufObject(Frame, fieldValue);
+        data.setFramesList([frameData]);
+        return;
+      }
+      if (
+        fieldType === ModelTypeField.DataType.FRAME &&
+        Array.isArray(fieldValue)
+      ) {
+        const framesList = fieldValue.map((each) => {
+          return fromPartialProtobufObject(Frame, each);
+        });
+        data.setFramesList(framesList);
+        return;
+      }
+      if (!Array.isArray(fieldValue)) {
+        // Unknown object just store it as string
+        updatedFieldValue = JSON.stringify(fieldValue);
+      }
     }
 
     const fieldValueStruct = Struct.fromJavaScript({
